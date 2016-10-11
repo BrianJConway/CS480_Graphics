7bb53bbdbdf4e9e83ba96a223c3520b9d210a7d8
BrianJConway <brianc@nevada.unr.edu>
2016-10-10 21:16:03 -0700

    shaders now work with textures

--- PA6/shaders/vertex.glsl
+++ PA6/shaders/vertex.glsl
@@ -1,9 +1,9 @@
  #version 330
           \
           layout (location = 0) in vec3 v_position; \
-          layout (location = 1) in vec3 v_color; \
+          layout (location = 1) in vec2 v_texture; \
           \
-          smooth out vec3 color; \
+          out vec2 texture; \
           \
           uniform mat4 projectionMatrix; \
           uniform mat4 viewMatrix; \
@@ -13,7 +13,7 @@
           { \
             vec4 v = vec4(v_position, 1.0); \
             gl_Position = (projectionMatrix * viewMatrix * modelMatrix) * v; \
-            color = v_color; \
+            texture = v_texture; \
           } \
           
           
