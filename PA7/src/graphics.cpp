168072179fd3af9bf3fefb42accfee8f41968047
BrianJConway <brianc@nevada.unr.edu>
2016-10-15 20:41:55 -0700

    Creates planets, updates planets, draws planets

--- PA7/src/graphics.cpp
+++ PA7/src/graphics.cpp
@@ -19,8 +19,6 @@ bool Graphics::Initialize(int width, int height, std::string fNames[] )
 {
   // Initialize function/variables
   bool validObj;
-  vector<Vertex> vertices;
-  vector<unsigned int> indices;
   
   // Used for the linux OS
   #if !defined(__APPLE__) && !defined(MACOSX)
@@ -53,9 +51,9 @@ bool Graphics::Initialize(int width, int height, std::string fNames[] )
     printf("Camera Failed to Initialize\n");
     return false;
   }
-
-  // Load the model
-  m_object = new Model( fNames[ 2 ] );
+         
+  // Load models for solar system
+  loadPlanets();
 
   // Set up the shaders
   m_shader = new Shader( fNames );
@@ -117,14 +115,89 @@ bool Graphics::Initialize(int width, int height, std::string fNames[] )
   return true;
 }
 
+void Graphics::loadPlanets()
+   {
+    // initialize function/variables
+    int index;
+    
+    // Loop through sun, planets, and pluto
+    for( index = 0; index < 10; index++ )
+       {
+        // Check planet index
+        switch( index )
+           {
+            // Check for sun index
+            case 0:
+              planets[ index ] = new planet( index, "sun.obj", 1.0, 26.24, 0, 0, -1  ); 
+              break;  
+  
+            // Check for mercury index
+            case 1:
+              planets[ index ] = new planet( index, "mercury.obj", 0.0056, 58.65, 0, 0.39, .24 ); 
+              break;  
+
+            // Check for venus index
+            case 2:
+              planets[ index ] = new planet( index, "venus.obj", 0.0143, 243, 0, 0.72, 0.62 ); 
+              break;
+
+            // Check for earth index
+            case 3:
+              planets[ index ] = new planet( index, "earth.obj", 0.0151, 1, 1, 1, 1 ); 
+              break;  
+            
+            // Check for mars index
+            case 4:
+              planets[ index ] = new planet( index, "mars.obj", 0.008, 1.03, 2, 1.52, 1.88 ); 
+              break;
+
+            // Check for jupiter index
+            case 5:
+              planets[ index ] = new planet( index, "jupiter.obj", 0.1652, 0.41, 67, 5.2, 11.86 ); 
+              break;  
+  
+            // Check for saturn index
+            case 6:
+              planets[ index ] = new planet( index, "saturn.obj", 0.1388, 0.44, 62, 9.54, 29.46 ); 
+              break;  
+
+            // Check for uranus index
+            case 7:
+              planets[ index ] = new planet( index, "uranus.obj", 0.0591, 0.72, 27, 19.18, 84.01 ); 
+              break;
+
+            // Check for neptune index
+            case 8:
+              planets[ index ] = new planet( index, "neptune.obj", 0.0573, 0.72, 14, 30.06, 164.8 ); 
+              break;  
+            
+            // Otherwise, assume pluto index
+            default:
+              planets[ index ] = new planet( index, "pluto.obj", 0.0027, 6.38, 5, 39.44, 247.7 ); 
+              break;
+           }
+       
+       }
+   }
+
 void Graphics::Update(unsigned int dt)
 {
-  // Update the object
-  m_object->Update(dt);
+  // Initialize function/variables
+  int index;
+  
+  // Loop through all objects in system
+  for( index = 0; index < 10; index++ )
+     {
+      // Update the current object
+      planets[ index ]->Update(dt);
+     }
 }
 
 void Graphics::Render()
 {
+  // Initialize function/variables
+  int index;
+  
   //clear the screen
   glClearColor(0.0, 0.0, 0.2, 1.0);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -136,9 +209,14 @@ void Graphics::Render()
   glUniformMatrix4fv(m_projectionMatrix, 1, GL_FALSE, glm::value_ptr(m_camera->GetProjection())); 
   glUniformMatrix4fv(m_viewMatrix, 1, GL_FALSE, glm::value_ptr(m_camera->GetView())); 
 
-  // Render the object
-  glUniformMatrix4fv(m_modelMatrix, 1, GL_FALSE, glm::value_ptr(m_object->getModel()));
-  m_object->Draw();
+  // Loop through all objects in the system
+  for( index = 0; index < 10; index++ )
+     {
+      // Render the current object
+      glUniformMatrix4fv(m_modelMatrix, 1, GL_FALSE, glm::value_ptr(planets[index]->getModel()));
+      planets[ index ]->Draw( m_modelMatrix );
+     }
+  // end loop
 
   // Get any errors from OpenGL
   auto error = glGetError();
