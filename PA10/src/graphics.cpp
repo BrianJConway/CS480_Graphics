9624d2933232419d2ef3f30a4eb5b93842e1a4dd
Stefan Stukelja <sstukelja@nevada.unr.edu>
2016-11-20 19:21:59 -0800

    Models

--- PA10/src/graphics.cpp
+++ PA10/src/graphics.cpp
@@ -67,7 +67,7 @@ bool Graphics::Initialize(int width, int height, std::string fNames[] )
   string objFile = "PA10PinballMachineNoBumper.obj";
   m_ground = new Ground( objFile, dynamicsWorld );
 
-  objFile = "PA10Ball.obj";
+  objFile = "PA10Ball_new.obj";
   m_sphere = new Sphere( objFile, dynamicsWorld );
 
   //objFile = "spring.obj";
